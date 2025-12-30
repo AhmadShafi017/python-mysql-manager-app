96f29bde3f18fe7fe48b4b26e1ad2513d517dfd9
AhmadShafi017 <arif.social017@gmail.com>
2025-12-30 11:02:46 +0600

    Implemented file-based data upload with schema validation (CSV / Excel / JSON)
    Implemented file-based data ingestion with validation against database
    schema. Supports CSV, Excel, and JSON formats using shared validation
    and insertion logic. Designed for clean extensibility and analytics readiness.

--- upload_from_csv.py
+++ upload_from_csv.py
@@ -1,12 +1,66 @@
+import csv
 import stack as sk
 
 def upload_from_csv(connection , db, stack):
-    x = input("Upload CSV")
+    filename = input("Enter the CSV filename (without extension):").strip()
+    full_path = f"{filename}.csv"
+    
 
     try:
-        with open("x.csv") as f:
-            print(f.read)
-    except Exception as e:
-        print(f"\n{x}.csv File must be in the same folder\n",e)
+        #----------READ CSV----------------------------
+        with open(full_path, newline="", encoding="utf-8") as file:
+            reader = csv.DictReader(file)
+            csv_columns = reader.fieldnames
+            
+            if not csv_columns:
+                print("❌ CSV file has no header.")
+                sk.go_back(stack)
+                return
+            
 
-    sk.go_back(stack)
+
+            #--------------READ DB TABLE COLUMNS---------
+            cursor = connection.cursor()
+            cursor.execute("DESCRIBE users")
+            table_columns = [row[0] for row in cursor.fetchall()]
+
+
+            #Remove auto-increment ID
+            if "id" in table_columns:
+                table_columns.remove("id")
+
+            #---------COMPARE COLUMNS-----------
+            if set(csv_columns) != set(table_columns):
+                print("❌ Column mismatch!")
+                print(f"CSV columns   : {csv_columns}")
+                print(f"Table columns : {table_columns}")
+                sk.go_back(stack)
+                return
+            
+            #--------INSERT DATA-------------
+            query = """
+                INSERT INTO users (name,age,email)
+                VSLUES (%s,%s,%s)
+            """
+
+            data = []
+            for row in reader:
+                data.append((
+                    row["name"],
+                    int(row["age"]),
+                    row["email"]
+                ))
+
+            cursor.executemany(query,data)
+            connection.commit()
+            print(f"✅ {len(data)} records uploaded successfully!")
+
+    except FileNotFoundError:
+        print(f"❌ File '{full_path}' not found.")
+
+    except Exception as e:
+        print("❌ Error while uploading CSV:", e)
+
+    finally:
+        sk.go_back(stack)
+
